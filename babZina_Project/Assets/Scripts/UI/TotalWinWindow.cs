f06facd3ec8d0ead81651b1bdc201b479a538ed1
Alena Karpova <95620549+AlenaKarp@users.noreply.github.com>
2024-08-18 20:34:30 +0300

    total win window

---
+++ babZina_Project/Assets/Scripts/UI/TotalWinWindow.cs
@@ -0,0 +1,29 @@
+﻿//this empty line for UTF-8 BOM header
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TotalWinWindow : MonoBehaviour
+{
+    [SerializeField] private Button continueButton;
+    [SerializeField] private GameObject window;
+
+    private void Awake()
+    {
+        GetComponentInParent<UIManager>().OnWinWindowOpen += OnWinWindowOpen;
+
+        continueButton.onClick.AddListener(OnContinue);
+    }
+
+    private void OnWinWindowOpen()
+    {
+        window.SetActive(true);
+    }
+
+    private void OnContinue()
+    {
+        Destroy(gameObject);
+    }
+}
