d77a671186cd61e51056053a2513ac74c20ca201
Alena Karpova <95620549+AlenaKarp@users.noreply.github.com>
2024-09-09 22:30:39 +0300

    dis button

---
+++ babZina_Project/Assets/Scripts/UI/MainMenuUI/DiscordButton.cs
@@ -0,0 +1,20 @@
+﻿//this empty line for UTF-8 BOM header
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiscordButton : MonoBehaviour
+{
+    [SerializeField] private Button button;
+
+    private void Awake()
+    {
+        button.onClick.AddListener(OnClick);
+    }
+
+    private void OnClick()
+    {
+        Application.OpenURL("https://discord.gg/4AnSY8CT");
+    }
+}
