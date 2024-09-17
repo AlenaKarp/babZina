e25942a215cffc24eea7218962182d9211fcfe6e
Alena Karpova <95620549+AlenaKarp@users.noreply.github.com>
2024-09-18 00:03:00 +0300

    remake mainMenu

---
+++ babZina_Project/Assets/Scripts/UI/MainMenuUI/ChangeLanguageButton.cs
@@ -0,0 +1,26 @@
+﻿//this empty line for UTF-8 BOM header
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Localization.Settings;
+using UnityEngine.UI;
+
+public class ChangeLanguageButton : MonoBehaviour
+{
+    private bool active = false;
+
+    public void ChangeLocale(int localeID)
+    {
+        if(active == true)
+        {
+            return;
+        } 
+
+        StartCoroutine(SetLocale(localeID));
+    }
+
+    private IEnumerator SetLocale(int localeID)
+    {
+        yield return LocalizationSettings.InitializationOperation;
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[localeID];
+    }
+}
