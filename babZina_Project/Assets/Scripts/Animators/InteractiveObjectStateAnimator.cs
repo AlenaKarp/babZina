e6b2fb88c4b7bcc691877baae5a883395b6aed67
Alena Karpova <95620549+AlenaKarp@users.noreply.github.com>
2024-10-20 20:08:26 +0300

    day1

---
+++ babZina_Project/Assets/Scripts/Animators/InteractiveObjectStateAnimator.cs
@@ -0,0 +1,27 @@
+﻿//this empty line for UTF-8 BOM header
+using UnityEngine;
+
+public class InteractiveObjectStateAnimator : MonoBehaviour
+{
+    [SerializeField] InteractiveObject interactioveObject;
+    [SerializeField] private Animator animator;
+
+    private const string animatorParameter_StateID_Int_Name = "ID";
+
+    private static readonly int animatorParameter_StateID_Int_Id = Animator.StringToHash(animatorParameter_StateID_Int_Name);
+
+    private void Awake()
+    {
+        interactioveObject.CurrentState.OnValueChanged += OnStateChanged;
+    }
+
+    private void OnDestroy()
+    {
+        interactioveObject.CurrentState.OnValueChanged -= OnStateChanged;
+    }
+
+    private void OnStateChanged(IInteractiveObject.State state)
+    {
+        animator.SetInteger(animatorParameter_StateID_Int_Id, (int)state);
+    }
+}
